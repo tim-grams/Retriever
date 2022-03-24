9f6fa7b631ec63aabfbe52454e548220159afb3c
Tim Grams <t.grams339@gmail.com>
2022-03-24 11:09:17 +0100

    added cli.py

---
+++ cli.py
@@ -0,0 +1,73 @@
+import logging
+import argparse
+
+LOGGER = logging.getLogger('cli')
+
+
+def _download_data(args):
+    LOGGER.info('Download of data started')
+
+
+def _get_parser():
+    """ Sets up a command line interface.
+    Args:
+        parser (ArgumentParser): Argument parser.
+    """
+    logging_args = argparse.ArgumentParser(add_help=False)
+    logging_args.add_argument('-v', '--verbose', action='count', default=0)
+    logging_args.add_argument('-l', '--logfile')
+    parser = argparse.ArgumentParser(description='Face-Hunter Command Line Interface',
+                                     parents=[logging_args])
+    subparsers = parser.add_subparsers(title='action', help='Action to perform')
+
+    # Parser to search for videos with an entity
+    data_download = subparsers.add_parser('download', help='Downloads the specified dataset in the /data/ folder')
+    data_download.add_argument('--dataset', help='Name of the dataset', type=str, default=None)
+    data_download.set_defaults(action=_download_data)
+
+    return parser
+
+
+def _logging_setup(verbosity: int = 1, logfile: str = None):
+    """ Sets up a logging interface.
+    Args:
+        verbosity (int): Verbosity level of the logging output.
+        logfile (str): Location of a logfile in which the logs can be saved.
+    """
+    logger = logging.getLogger()
+    log_level = (2 - verbosity) * 10
+    fmt = '%(asctime)s - %(levelname)s - %(module)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+    logger.setLevel(log_level)
+    logger.propagate = False
+
+    if logfile:
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    else:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(log_level)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    logging.getLogger('retriever:worker').setLevel(logging.INFO)
+
+
+def main():
+    parser = _get_parser()
+    args = parser.parse_args()
+
+    _logging_setup(args.verbose, args.logfile)
+
+    if not hasattr(args, 'action'):
+        parser.print_help()
+        parser.exit()
+
+    args.action(args)
+
+
+if __name__ == '__main__':
+    main()
