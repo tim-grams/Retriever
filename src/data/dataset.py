ab242b9881f5d21c5e6a04db5993c4249a2f5b1e
Tim Grams <t.grams339@gmail.com>
2022-03-24 11:12:28 +0100

    update

---
+++ src/data/dataset.py
@@ -0,0 +1,3 @@
+
+def download_dataset1():
+    pass
---
+++ src/pipeline.py
@@ -0,0 +1,6 @@
+
+class Pipeline(object):
+    """ Class to combine the different download, preprocessing, modeling and evaluation steps. """
+
+    def __init__(self):
+        pass
