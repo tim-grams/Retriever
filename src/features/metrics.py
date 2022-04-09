897e7607af29e773d33699706ef3cfc1eb66c4d5
Tim Grams <t.grams339@gmail.com>
2022-04-09 20:12:45 +0300

    added some more features

--- src/features/metrics.py
+++ src/features/metrics.py
@@ -1,4 +1,7 @@
 from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances, manhattan_distances
+from src.preprocessing.preprocessing import tokenization
+import nltk
+from textblob import TextBlob
 
 
 def cosine_similarity_score(embedding_1, embedding_2):
@@ -20,3 +23,36 @@ def jaccard(token_vector_1, token_vector_2):
         return len(intersect) / len(union)
     except ZeroDivisionError:
         return 0
+
+
+def characters(sentence: str):
+    return len(sentence.replace(' ', ''))
+
+
+def words(preprocessed_tokens: list):
+    return len(preprocessed_tokens)
+
+
+def POS(sentence):
+    tokens = list(tokenization(sentence))
+    tags = nltk.pos_tag(tokens)
+    nouns = len([tag[0] for tag in tags if tag[1].startswith('NN')])
+    adj = len([tag[0] for tag in tags if tag[1].startswith('JJ')])
+    verbs = len([tag[0] for tag in tags if tag[1].startswith('VB')])
+    return nouns, adj, verbs
+
+
+def subjectivity(sentence):
+    return TextBlob(sentence).sentiment.subjectivity
+
+
+def polarisation(sentence):
+    return TextBlob(sentence).sentiment.polarity
+
+
+def named_entities(sentence):
+    pass
+
+
+def stopwords(sentence):
+    pass
