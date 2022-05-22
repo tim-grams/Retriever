4e51222002f97188a453277852be46eacf7a290b
Tim Grams <t.grams339@gmail.com>
2022-05-22 11:08:30 +0200

    small change

--- src/embeddings/glove.py
+++ src/embeddings/glove.py
@@ -1,7 +1,7 @@
 from flair.embeddings import WordEmbeddings
 from flair.data import Sentence
 import pandas as pd
-from src.utils.utils import check_path_exists, save, load
+from src.utils.utils import check_path_exists, save
 import os
 from tqdm import tqdm
 import torch
@@ -9,20 +9,20 @@ from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 
 class Glove(object):
-    ''' A class to create glove embeddings.
+    """ A class to create glove embeddings.
 
     Methods:
     transform(text_in_tokens: pd.Series, store: str = None)
         Transform series of preprocessed tokens to glove embeddings
-    '''
+    """
     glove = None
 
     def __init__(self):
-        ''' Constructs glove object using a pretrained model. ''' 
+        """ Constructs glove object using a pretrained model. """
         self.glove = WordEmbeddings('glove')
 
     def transform(self, text_in_tokens: pd.Series, store: str = None):
-        ''' Transform series of preprocessed tokens to glove embeddings.
+        """ Transform series of preprocessed tokens to glove embeddings.
     
         Args:
             text_in_tokens (pd.Series): Series of preprocessed tokens
@@ -30,7 +30,7 @@ class Glove(object):
         Returns:
             glove_vec (list): List containing glove embeddings
 
-        '''      
+        """
         glove_vec = []
 
         for line in tqdm(text_in_tokens):
@@ -49,30 +49,3 @@ class Glove(object):
             save(glove_vec, store)
 
         return glove_vec
-    
-    
-"""   def transform_tfidfweighted(self, text_in_tokens: pd.Series, tf_idf_weights: pd.Series, store: str = None ):
-
-        tfidf_glove_vec = [] # the tfidf-ft for each sentence/review is stored in this list
-        
-        for count, line in enumerate(tqdm(text_in_tokens)): # for each sentence
-            detokenized = TreebankWordDetokenizer().detokenize(line)
-            sentence = Sentence(detokenized)
-            self.glove.embed(sentence)
-            input = torch.empty(sentence[0].embedding.size())
-            token_per_sentence = torch.zeros_like(input)
-            tfidf_weights = tf_idf_weights.at[count]
-            weight_sum = 0
-
-            for count2 , token in enumerate(sentence): # for each word in a sentence
-                    vec = token.embedding
-                    raw_token = line[count2]
-                    # obtain the tf_idfidf of a word in a sentence
-                    tfidf = tfidf_weights[raw_token]
-                    token_per_sentence = torch.add(token_per_sentence, (vec * tfidf))
-                    weight_sum += tfidf
-                    
-                    token_per_sentence = torch.add(token_per_sentence, vec)
-
-            torch.div(token_per_sentence, weight_sum)
-            tfidf_glove_vec.append(token_per_sentence.numpy()) """
